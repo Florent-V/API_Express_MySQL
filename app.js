e4a2d3ccb3705504a4a5c23dcc74364d44e478c2
Florent-Vasseur <vasseurflorent@gmail.com>
2023-02-15 13:31:11 +0100

    quete 3 ok

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 
 const app = express();
+app.use(express.json());
 
 const port = process.env.APP_PORT ?? 5000;
 
@@ -16,6 +17,13 @@ const userHandlers = require("./userHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
+
+app.post("/api/movies", movieHandlers.addMovie);
+app.post("/api/users", userHandlers.addUser);
+
+
+
+
 app.get("/api/users", userHandlers.getUsers);
 app.get("/api/users/:id", userHandlers.getUserById);
 
